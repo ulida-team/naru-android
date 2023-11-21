760eae3f6daf6e8d630d0a42d325f8d45fb61264
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-11-21 13:49:20 +0900

    ✨:: create custom plugin to use hilt

---
+++ build-logic/convention/src/main/java/AndroidHiltPlugin.kt
@@ -0,0 +1,19 @@
+import com.ulida.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidHiltPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.google.devtools.ksp")
+                apply("dagger.hilt.android.plugin")
+            }
+            dependencies {
+                "implementation"(libs.findLibrary("hilt-android").get())
+                "ksp"(libs.findLibrary("hilt-compiler").get())
+            }
+        }
+    }
+}
