6a3c1e88ed9790e79117e451b7a503bf81cb32be
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-11-29 19:31:17 +0900

    Add try catch and write function that create libs

--- script/generateModule.exs
+++ script/generateModule.exs
@@ -21,61 +21,80 @@ defmodule ModuleGenerator do
     IO.puts("create feature module")
     File.mkdir_p!("#{moduleName}")
     File.cd!("#{moduleName}")
+    IO.puts("---------------------------")
     generateIgnore()
     generateGradle()
     generateConsumer()
     generateProguard()
+    generateLibs()
   end
 
   def generateIgnore do
     #    generate ignore
-    File.write(".gitignore", "/build")
-    IO.puts("gitignore success")
+    case File.write(".gitignore", "/build") do
+      :ok -> IO.puts("gitignore success")
+      :error -> IO.puts("gitignore error")
+    end
   end
 
   def generateGradle do
     #    generate build.gradle.kts
-    File.write("build.gradle.kts",
-      """
-      @file:Suppress("DSL_SCOPE_VIOLATION")
-      plugins {
-          alias(libs.plugins.naru.android.application.feature)
-      }
-      """)
-    IO.puts("build.gradle success")
+    case File.write("build.gradle.kts",
+           """
+        @file:Suppress("DSL_SCOPE_VIOLATION")
+        plugins {
+            alias(libs.plugins.naru.android.application.feature)
+        }
+           """)
+      do
+      :ok -> IO.puts("build.gradle success")
+      :error -> IO.puts("build.gradle error")
+    end
   end
 
   def generateConsumer do
     #    generate consumer-rules
-    File.write("consumer-rules.pro", "")
-    IO.puts("consumer-rules success")
+    case File.write("consumer-rules.pro", "") do
+      :ok -> IO.puts("consumer-rules success")
+      :error -> IO.puts("consumer-rules error")
+    end
   end
 
   def generateProguard do
-    File.write("proguard-rules.pro",
-    """
-    # Add project specific ProGuard rules here.
-    # You can control the set of applied configuration files using the
-    # proguardFiles setting in build.gradle.
-    #
-    # For more details, see
-    #   http://developer.android.com/guide/developing/tools/proguard.html
+    case File.write("proguard-rules.pro",
+           """
+           # Add project specific ProGuard rules here.
+           # You can control the set of applied configuration files using the
+           # proguardFiles setting in build.gradle.
+           #
+           # For more details, see
+           #   http://developer.android.com/guide/developing/tools/proguard.html
 
-    # If your project uses WebView with JS, uncomment the following
-    # and specify the fully qualified class name to the JavaScript interface
-    # class:
-    #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
-    #   public *;
-    #}
+           # If your project uses WebView with JS, uncomment the following
+           # and specify the fully qualified class name to the JavaScript interface
+           # class:
+           #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
+           #   public *;
+           #}
 
-    # Uncomment this to preserve the line number information for
-    # debugging stack traces.
-    #-keepattributes SourceFile,LineNumberTable
+           # Uncomment this to preserve the line number information for
+           # debugging stack traces.
+           #-keepattributes SourceFile,LineNumberTable
 
-    # If you keep the line number information, uncomment this to
-    # hide the original source file name.
-    #-renamesourcefileattribute SourceFile
-    """)
+           # If you keep the line number information, uncomment this to
+           # hide the original source file name.
+           #-renamesourcefileattribute SourceFile
+           """) do
+      :ok -> IO.puts("proguard success")
+      :error -> IO.puts("proguard error")
+    end
+  end
+
+  def generateLibs() do
+    case File.mkdir_p!("libs") do
+      :ok -> IO.puts("libs success")
+      :error -> IO.puts("libs error")
+    end
   end
 end
 
