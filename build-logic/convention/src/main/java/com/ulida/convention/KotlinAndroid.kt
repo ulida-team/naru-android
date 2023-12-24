ce08f316aaf0221f51824883b48cb24434ff6735
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-12-24 13:56:56 +0900

    ✨:: Create configure to setting android dependencies

---
+++ build-logic/convention/src/main/java/com/ulida/convention/KotlinAndroid.kt
@@ -0,0 +1,24 @@
+package com.ulida.convention
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *>,
+) {
+    commonExtension.apply {
+        compileSdk = 34
+
+        defaultConfig {
+            minSdk = 21
+
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+    }
+}
